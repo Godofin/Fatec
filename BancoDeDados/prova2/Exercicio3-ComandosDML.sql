e9eb3fc915c425faf6deedcbc929a3f8cc4ac7c9
Guilherme Lavezzo <65193596+Godofin@users.noreply.github.com>
2022-06-10 16:27:24 -0300

    Update Exercicio3-ComandosDML.sql

--- BancoDeDados/prova2/Exercicio3-ComandosDML.sql
+++ BancoDeDados/prova2/Exercicio3-ComandosDML.sql
@@ -1,3 +1,16 @@
 /*1 Retorne todos Clientes do estado de São Paulo*/
 
-select * from cliente where regiao = 'sp';
+select * from cliente where regiao = 'SP';
+
+/*2*/
+select razao_social, regiao from cliente where pais = 'brazil';
+
+/*3*/
+select * from cliente where pais = 'Brazil' and cargo like '%manager%';
+
+/*4*/
+select count(cidade) as conta_cidade from cliente group by cidade;
+
+/*5*/
+select * from cliente where razao_social like'%KIDPX' 
+update cliente set contato = 'Durant, Paul' where id_cliente = 21;
